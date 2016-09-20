c6a356ee8cdd01c4d6d9e722c79ad0b002ba7cfe
Yoseph Anderson <yrganderson@gmail.com>
2016-09-20 11:17:57 -0600

    bestbuy store locator * 17 results * long_name, city, distance, phone, store_type

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,16 @@
 class SearchController < ApplicationController
   def index
-    response = Faraday.get("https://api.bestbuy.com/v1/stores((area(80202,25)))?apiKey=#{ENV["API_KEY"]}&pageSize=20&callback=JSON_CALLBACK&format=json")
-    # data = JSON.parse(response.body.delete("JSON_CALLBACK" ) )
-    require 'pry'; binding.pry
+    response = Faraday.get("https://api.bestbuy.com/v1/stores((area(80202,25)))?apiKey=#{ENV["API_KEY"]}&pageSize=20&format=json")
+    data = JSON.parse(response.body)["stores"]
+    @stores = data.map do |store|
+       store_data = { long_name: store["longName"],
+                      city: store["city"],
+                      distance: store["distance"],
+                      phone_number: store["phone"],
+                      store_type: store["storeType"] }
+
+      OpenStruct.new(store_data)
+    end
+    # stores have 17 results
   end
 end
